22d48f8f5daee3ce1431f611beed4b22921ea6ea
hayrapetyanhasmik <hasyokahasiko7@gmail.com>
2022-04-06 01:37:01 +0400

    Added filtering tests using Mocha

--- cypress/integration/todomvc-filtering.spec.js
+++ cypress/integration/todomvc-filtering.spec.js
@@ -8,31 +8,25 @@ describe('filtering', function(){
         cy.get('.new-todo').type('Learn JavaScript{enter}')
         cy.get('.new-todo').type('Use Cypress{enter}')
 
-        cy.get('.todo-List li:nth-child(2).toggle').click()
+        cy.get('.todo-list li:nth-child(2) .toggle').click()
     })
 
     it('should filter "Active" todos', function(){
         cy.contains('Active').click()
 
-        cy.get('.todo-List li').should('have.length', 2)
+        cy.get('.todo-list li').should('have.length', 2)
     })
 
     it('should filter "Completed" todos', function(){
         cy.contains('Completed').click()
 
-        cy.get('.todo-List li').should('have.length', 1)
-    })
-
-    it('should filter "Active" todos', function(){
-        cy.contains('Active').click()
-
-        cy.get('.todo-List li').should('have.length', 2)
+        cy.get('.todo-list li').should('have.length', 1)
     })
 
     it('should filter "All" todos', function(){
         cy.contains('All').click()
 
-        cy.get('.todo-List li').should('have.length', 3)
+        cy.get('.todo-list li').should('have.length', 3)
     })
 
 })
