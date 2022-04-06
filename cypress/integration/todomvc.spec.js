cd43e2ca43f510d5c4063f84967836b1f537c9d2
hayrapetyanhasmik <hasyokahasiko7@gmail.com>
2022-04-07 02:17:00 +0400

    Modified todomvc.spec.js file for importing methods

--- cypress/integration/todomvc.spec.js
+++ cypress/integration/todomvc.spec.js
@@ -1,23 +1,41 @@
 /// <reference types="cypress" />
 
-describe('todo actions', function(){ 
-    beforeEach(function() {
-        cy.visit('http://todomvc-app-for-testing.surge.sh/')
-        cy.get('.new-todo', {timeout: 4500}).type('Learn Cypress{enter}')
+import {
+    navigate,
+    addTodo,
+    validateTodoText,
+    toggleTodo,
+    clearCompleted,
+    validateTodoCompletedState,
+    validateToggleState,
+    validateNumberOfTodosShown,
+  } from '../page-objects/todo-page'
+  
+  describe('todo actions', () => {
+    beforeEach(() => {
+      navigate()
+  
+      addTodo('Clean room')
     })
-    it('should add a new todo to the list', function(){
-       cy.get('label').should('have.text', 'Learn Cypress')
-       cy.get('.toggle').should('not.be.checked')
-})
-
-it('should mark a todo as completed', function(){
-    cy.get('.toggle').click()
-    cy.get('label').should('have.css', 'text-decoration-line', 'line-through')
-})
-
-it('should clear completed todos', function(){
-    cy.get('.toggle').click()
-    cy.contains("Clear").click()
-    cy.get('.todo-list').should('not.have.descendants', 'li')
-})
-})
+  
+    it('should add a new todo to the list', () => {
+      validateTodoText(0, 'Clean room')
+  
+      validateToggleState(0, false)
+    })
+  
+    describe('toggling todos', () => {
+      it('should toggle test correctly', () => {
+        toggleTodo(0)
+        validateTodoCompletedState(0, true)
+      })
+  
+      it('should clear completed', () => {
+        toggleTodo(0)
+  
+        clearCompleted()
+  
+        validateNumberOfTodosShown(0)
+      })
+    })
+  })
