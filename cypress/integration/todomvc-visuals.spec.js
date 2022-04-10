20dc9268a9208dea9987266609d4462115af07ee
hayrapetyanhasmik <hasyokahasiko7@gmail.com>
2022-04-11 02:39:12 +0400

    Added different browsers

--- cypress/integration/todomvc-visuals.spec.js
+++ cypress/integration/todomvc-visuals.spec.js
@@ -7,8 +7,16 @@ describe('visual validation', function(){
     })
     
     beforeEach(function(){
-        cy.eyesOpen({appName: 'TAU TodoMVC', batchName: 'TAU TodoMVC Hey!'})
+        cy.eyesOpen({appName: 'TAU TodoMVC', batchName: 'TAU TodoMVC Hey!',
+        browser: [
+            {name: 'chrome', width: 1024, height: 768},
+            {name: 'chrome', width: 800, height: 600},
+            {name: 'firefox', width: 1024, height: 768},
+            {deviceName: 'iPhone X'}
+        ]
+    })
     });
+    
     afterEach(function(){
         cy.eyesClose()
     });
