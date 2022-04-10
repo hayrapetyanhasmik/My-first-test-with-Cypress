72828771325b8ab2959c4308146e2c8e6c35f8f9
hayrapetyanhasmik <hasyokahasiko7@gmail.com>
2022-04-11 02:05:17 +0400

    Used Applitools

--- cypress/integration/todomvc-visuals.spec.js
+++ cypress/integration/todomvc-visuals.spec.js
@@ -5,11 +5,23 @@ describe('visual validation', function(){
     before(function(){
         todoPage.navigate()
     })
-
+    
+    beforeEach(function(){
+        cy.eyesOpen({appName: 'TAU TodoMVC', batchName: 'TAU TodoMVC Hey!'})
+    });
+    afterEach(function(){
+        cy.eyesClose()
+    });
     it('should look good', function(){
+        cy.eyesCheckWindow('empty todo list')
+
         todoPage.addTodo('Learn Cypress')
         todoPage.addTodo('Learn JavaScript')
 
+        cy.eyesCheckWindow('two todos')
+
         todoPage.toggleTodo(0)
+
+        cy.eyesCheckWindow('mark as completed')
     })
 })
